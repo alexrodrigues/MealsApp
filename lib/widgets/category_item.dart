834901c8df22d758b0a631233dc39f221d707cdf
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-10-16 08:07:47 -0300

    missing files

---
+++ lib/widgets/category_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoryItem extends StatelessWidget {
+  final String _title;
+  final Color _backgroundColor;
+
+  CategoryItem(this._title, this._backgroundColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Text(_title),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [_backgroundColor.withOpacity(0.7), _backgroundColor],
+          begin: Alignment.topLeft,
+          end: Alignment.topRight,
+        ),
+        borderRadius: BorderRadius.circular(8),
+      ),
+    );
+  }
+}
