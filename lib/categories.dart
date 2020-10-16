743ef416c5e6d01f23ebe4bc0b0c93167d6dee2a
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2020-10-16 07:52:30 -0300

    initial commit

---
+++ lib/categories.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatefulWidget {
+  @override
+  _CategoriesScreenState createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      children: [],
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 16,
+      ),
+    );
+  }
+}
